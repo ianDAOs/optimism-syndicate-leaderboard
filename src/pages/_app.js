5ddd2de91e293b60ac01b8d72541150cd11ca68f
ian <ian.john.lee@gmail.com>
2023-05-08 09:40:44 -0700

    Add Head content

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>Optimism Co-grants Leaderboard</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                {/* add favicon */}
+                {/* add social previews */}
+                <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 // persistent layout
