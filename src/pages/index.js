5420ab1d576d0ec11c5a3655a01a2da24dca0bac
ian <ian.john.lee@gmail.com>
2023-05-08 08:49:57 -0700

    Add index structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import Header from '../components/layout/Header';
+import Content from '../components/layout/Content';
+import Footer from '../components/layout/Footer';
 
 export default function Home() {
     return (
         <div>
-            <p>Home</p>
+            <Header />
+            <Content />
+            <Footer />
         </div>
     );
 }
