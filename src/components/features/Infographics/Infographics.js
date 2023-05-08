de9f875e92634dd1978b61004217991dd9f394ba
ian <ian.john.lee@gmail.com>
2023-05-08 10:41:47 -0700

    Add Infographics files

--- src/components/features/Infographics/Infographics.js
+++ src/components/features/Infographics/Infographics.js
@@ -1,7 +1,13 @@
+import Button from "../../ui/Button/Button";
+
 export default function Infographics() {
     return (
         <div>
-            <p>Infographics</p>
+            <p>Infographics for Season 4</p>
+            <p>Total co-grantors</p>
+            <p>Total co-grants</p>
+            <p>Time left</p>
+            <Button />
         </div>
     );
 }
