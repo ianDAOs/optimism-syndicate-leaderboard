c6fa84ac082a4ee7e5be45f749a1d1f1845240d1
ian <ian.john.lee@gmail.com>
2023-05-11 09:29:25 -0700

    Add Leaderboard option

--- src/components/features/leaderboard/LeaderboardOptions.js
+++ src/components/features/leaderboard/LeaderboardOptions.js
@@ -3,8 +3,9 @@ import LeaderboardOption from "./LeaderboardOption";
 export default function LeaderboardOptions() {
     return (
         <>
-            <LeaderboardOption title="Amount" />
+            <LeaderboardOption title="Total Amount" />
             <LeaderboardOption title="Contributions" />
+            <LeaderboardOption title="Largest Contribution" />
             <LeaderboardOption title="Contributor Number" />
         </>
     );
