802cd5ce4622ed1ae6b833e0fa6269d62909745d
ian <ian.john.lee@gmail.com>
2023-05-11 10:46:13 -0700

    Reorder infographics labels

--- src/components/features/infographics/Infographics.js
+++ src/components/features/infographics/Infographics.js
@@ -21,16 +21,16 @@ export default function Infographics() {
         <section>
             <p>Infographics</p>
             <p>Total Contribution Amount</p>
-            <p>Total Contributions: {data ? data.totalSupply : ("Loading...")}</p>
+            <p>{data ? data.totalSupply : ("Loading...")} Total Contributions</p>
             <p>Largest Contribution</p>
             <p>Number of Contributors</p>
             <p>Most Contributions</p>
             <p>Implied Impact</p>
             <p>Safe Address</p>
             <p>ERC-721M Contract</p>
-            <p>Address: {data ? data.address : ("Loading...")}</p>
-            <p>Name: {data ? data.name : ("Loading...")}</p>
-            <p>Symbol: {data ? data.symbol : ("Loading...")}</p>
+            <p>{data ? data.address : ("Loading...")} Address</p>
+            <p>{data ? data.name : ("Loading...")} Name</p>
+            <p>{data ? data.symbol : ("Loading...")} Symbol</p>
         </section>
     );
 }
