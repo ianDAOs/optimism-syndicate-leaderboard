7b52e7aa4c5e8276613a55b8cc1f6d802ef269f3
ian <ian.john.lee@gmail.com>
2023-05-12 08:53:24 -0700

    Add button padding top

--- src/components/features/leaderboard/LeaderboardNav.js
+++ src/components/features/leaderboard/LeaderboardNav.js
@@ -1,7 +1,9 @@
 export default function LeaderboardNav() {
     return (
-        <button className="rounded py-1 px-2 bg-sky-500 hover:bg-sky-700 text-sm text-white font-medium">
-            Next 25
-        </button>
+        <div className="pt-8">
+            <button className="rounded py-1 px-2 bg-sky-500 hover:bg-sky-700 text-sm text-white font-medium">
+                Next 25
+            </button>
+        </div>
     );
 }
